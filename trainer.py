0b24d547968cb90a616953655d2b114484b04789
John S <tinco7@aol.com>
2021-01-29 18:04:19 -0800

    cleaned up trainer

--- trainer.py
+++ trainer.py
@@ -9,89 +9,89 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import tree
 from joblib import dump
-#show more columns
+#import requests
 pd.set_option('display.max_columns',10)
+#Set stock to train on
+stock = 'MSFT'
+#Finnhub Info
+
+#Clean data for training
 def calcRating(row):
     index = row.name
     index = index + pd.DateOffset(days=20)
-    indexOffset = index.strftime('%Y-%m-%d')
-    rowPosShift = None
+    index_offset = index.strftime('%Y-%m-%d')
+    row_pos_shift = None
     tries = 0
     while (tries<10) & (index < (datetime.today() - pd.DateOffset(days=21))):
-        #print(tries)
         try:
-            rowPosShift = stockDataset.loc[[str(indexOffset)]]
+            row_pos_shift = stock_dataset.loc[[str(index_offset)]]
         except: 
             index = index + pd.DateOffset(days=1)
-            #print(index)
-            indexOffset = index.strftime('%Y-%m-%d')
+            index_offset = index.strftime('%Y-%m-%d')
             continue
         else:
             tries = 10
         tries += 1
     if index > (datetime.today() - pd.DateOffset(days=21)):
         return None
-    priceIncreased = True if rowPosShift.Open[0] > row.Open else False
-    goodRSI = True if row.RSI < 45 else False
-    goodTRIX = True if row.TRIX < 50 else False
-    badMACD = True if row.MACDh > 0.1 else False
-    goodMACD = True if row.MACDh < -0.1 else False
-    goodKDJ = True if row.KDJ < 50 else False
-    badRSI = True if row.RSI > 65 else False
-    if goodMACD & goodKDJ & goodRSI & goodTRIX & priceIncreased:
+    price_increased = True if row_pos_shift.Open[0] > row.Open else False
+    good_RSI = True if row.RSI < 45 else False
+    good_TRIX = True if row.TRIX < 50 else False
+    bad_MACD = True if row.MACDh > 0.1 else False
+    good_MACD = True if row.MACDh < -0.1 else False
+    good_KDJ = True if row.KDJ < 50 else False
+    bad_RSI = True if row.RSI > 65 else False
+    if good_MACD & good_KDJ & good_RSI & good_TRIX & price_increased:
         return 'Buy'
-    elif (not goodKDJ) & (not goodMACD) & (badRSI) & (not goodTRIX) & badMACD & (not priceIncreased):
+    elif (not good_KDJ) & (not good_MACD) & (bad_RSI) & (not good_TRIX) & bad_MACD & (not price_increased):
         return 'Sell'
     else:
         return 'Hold'
-#load data into dataframe and stock dataframe
-# stockDataset = pd.read_csv('D:/Dropbox/Dropbox (CSU Fullerton)/481/zStock-AI/Datasets/AMD.csv')
-# stockDataset.columns = ['Date','High','Low','Open','Close','Volume','AdjClose']
-msft = yf.Ticker("MSFT")
-stockDataset = msft.history(period='10y')
-print(stockDataset)
-stockDF = stockDataset.copy()
-#get data indicators
-stockDF = ss.StockDataFrame.retype(stockDF)
-macdh = stockDF['macdh']
-boll = stockDF['boll']
-kdjk = stockDF['kdjk']
-trix = stockDF['trix']
-rsi = stockDF['rsi_6']
+#Load data into dataframe and stock dataframe
+stock_data = yf.Ticker(stock)
+stock_dataset = stock_data.history(period='10y')
+stock_DF = stock_dataset.copy()
+#Det indicators
+stock_DF = ss.StockDataFrame.retype(stock_DF)
+macdh = stock_DF['macdh']
+boll = stock_DF['boll']
+kdjk = stock_DF['kdjk']
+trix = stock_DF['trix']
+rsi = stock_DF['rsi_6']
 #add indicators to dataframe
-stockDataset['RollingAvg'] = stockDataset.Volume.rolling(20).mean()
-stockDataset['RSI'] = rsi
-stockDataset['MACDh'] = macdh
-stockDataset['KDJ'] = kdjk
-stockDataset['TRIX'] = trix
-stockDataset = stockDataset.dropna()
-#print(stockDataset)
+stock_dataset['RollingAvg'] = stock_dataset.Volume.rolling(20).mean()
+stock_dataset['RSI'] = rsi
+stock_dataset['MACDh'] = macdh
+stock_dataset['KDJ'] = kdjk
+stock_dataset['TRIX'] = trix
+stock_dataset = stock_dataset.dropna()
+#print(stock_dataset)
 
 #Normalize Data
-stockDataset["TRIX"]=((stockDataset["TRIX"]-stockDataset["TRIX"].min())/(stockDataset["TRIX"].max()-stockDataset["TRIX"].min()))*100
-stockDataset["RollingAvg"]=((stockDataset["RollingAvg"]-stockDataset["RollingAvg"].min())/(stockDataset["RollingAvg"].max()-stockDataset["RollingAvg"].min()))*20
-stockDataset["Volume"]=((stockDataset["Volume"]-stockDataset["Volume"].min())/(stockDataset["Volume"].max()-stockDataset["Volume"].min()))*100
-stockDataset["KDJ"]=((stockDataset["KDJ"]-stockDataset["KDJ"].min())/(stockDataset["KDJ"].max()-stockDataset["KDJ"].min()))*100
+stock_dataset["TRIX"]=((stock_dataset["TRIX"]-stock_dataset["TRIX"].min())/(stock_dataset["TRIX"].max()-stock_dataset["TRIX"].min()))*100
+stock_dataset["RollingAvg"]=((stock_dataset["RollingAvg"]-stock_dataset["RollingAvg"].min())/(stock_dataset["RollingAvg"].max()-stock_dataset["RollingAvg"].min()))*20
+stock_dataset["Volume"]=((stock_dataset["Volume"]-stock_dataset["Volume"].min())/(stock_dataset["Volume"].max()-stock_dataset["Volume"].min()))*100
+stock_dataset["KDJ"]=((stock_dataset["KDJ"]-stock_dataset["KDJ"].min())/(stock_dataset["KDJ"].max()-stock_dataset["KDJ"].min()))*100
 
 #calculate good buy times and add column to dataframe
-stockDataset['Rating'] = stockDataset.apply(lambda row: calcRating(row), axis=1)
+stock_dataset['Rating'] = stock_dataset.apply(lambda row: calcRating(row), axis=1)
 
 #Graph Data
-stockDataset.plot.line(y='Close')
-#lines = stockDataset.plot.line(y='Close')
-buyRating = stockDataset.query("Rating == 'Buy'")
+stock_dataset.plot.line(y='Close')
+#lines = stock_dataset.plot.line(y='Close')
+buyRating = stock_dataset.query("Rating == 'Buy'")
 #buyRating.plot(x=buyRating.index, y='Close', c='green', kind='scatter', sharex=True)
-sellRating = stockDataset.query("Rating == 'Sell'")
+sellRating = stock_dataset.query("Rating == 'Sell'")
 #sellRating.plot(x=sellRating.index, y='Close', c='red', kind='scatter', sharex=True)
-#plt.xticks(stockDataset.index,stockDataset['index'])
+#plt.xticks(stock_dataset.index,stock_dataset['index'])
 plt.plot(buyRating.index, buyRating.Close, 'g*')
 plt.plot(sellRating.index, sellRating.Close, 'r*')
 plt.show()
 
 #Machine Learning
-stockDataset = stockDataset.dropna()
-y = stockDataset['Rating']
-x = stockDataset.drop(['Rating','Open','Close','High','Low'], axis=1)
+stock_dataset = stock_dataset.dropna()
+y = stock_dataset['Rating']
+x = stock_dataset.drop(['Rating','Open','Close','High','Low'], axis=1)
 #print(x)
 
 #print(y)
