44797e87aa56596fb4f7946a966379b5302c7df7
John Scales <zirjohn97@gmail.com>
2025-02-08 14:34:04 -0800

    Add batch file for task scheduler to run test

---
+++ paper_trade.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Change directory to your project directory
+cd /d G:\Repos\Stock-Advizir\
+
+REM Activate the Python environment
+REM Replace 'your_env_name' with your actual environment name
+call G:\Repos\Stock-Advizir\stckadv_env\Scripts\activate
+
+REM Run the Python script
+python G:\Repos\Stock-Advizir\paper_test.py
+
+REM Deactivate the Python environment
+deactivate
